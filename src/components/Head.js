5a070a28c24fa0d41915458391e56d4d3302f622
Anchal <53646283+anchal15@users.noreply.github.com>
2024-03-08 10:48:05 +0000

    Added search feature

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,49 @@
-import React from "react";
-import { HAMBURGER_ICON, LOGO, USER_ICON } from "../utils/constant";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { HAMBURGER_ICON, LOGO, USER_ICON, YOUTUBE_SEARCH_API } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleSideNav } from "../utils/slices/appslice";
+import { cacheResult } from "../utils/slices/searchSlice";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
   const dispatch = useDispatch();
+  const searchCache = useSelector((store) => store.search);
+
+  /**
+   *  searchCache = {
+   *     "iphone": ["iphone 11", "iphone 14"]
+   *  }
+   *  searchQuery = iphone
+   */
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        if (searchQuery) {
+          getSearchSugsestions();
+        }
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSugsestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1].splice(0, 10));
+    dispatch(
+      cacheResult({
+        searchQuery: json[1].splice(0, 10),
+      })
+    );
+  };
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
@@ -15,9 +54,27 @@ const Head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
+        />
         <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">🔍</button>
-      </div>
+      </div>{" "}
+      {showSuggestions && (
+        <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestions.map((s) => (
+              <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                🔍 {s}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       <div className="col-span-1">
         <img className="h-8" alt="user" src={USER_ICON} />
       </div>
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./slices/appslice";
+import searchReducer from "./slices/searchSlice";
 
 const store = configureStore({
   reducer: {
     app: appReducer,
+    search: searchReducer,
   },
 });
 
